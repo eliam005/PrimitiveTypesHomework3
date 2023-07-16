ffd07eb9f92f053665dac4f3c6f6239312716617
Eliam <salcov.ilya@yandex.ru>
2023-07-16 14:39:02 +0300

    Changes Saved

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,23 @@ public class Main {
 
         // Задаем переменные для входных данных
 
-        int Balance = 250; // переменная для начального баланса
-        int TopUpAmount = 1500; // сумма пополнения
-        int BonusRubles = TopUpAmount / 100; // считаем бонусные рубли
-        int FinalBalance = Balance + TopUpAmount; // конечный баланс без бонусов
+        int balance = 250; // переменная для начального баланса
+        int topUpAmount = 1550; // сумма пополнения
+        int bonusRubles = topUpAmount / 100; // считаем бонусные рубли
+        int finalBalance = balance + topUpAmount; // конечный баланс без бонусов
 
 
         // Теперь определяем сумму пополнения, если выше N-ой суммыы, то выводим бонусы и итоговый баланс
         // ... Если иначе, то бонус нет, и мы просто складываем начальный баланс и сумму пополнения
 
-        if (TopUpAmount >= 1000) {
+        if (topUpAmount >= 1000) {
 
-            System.out.println("Ваши бонусы составят: " + BonusRubles + " рублей.");
-            System.out.println("Ваш итоговый баланс равен: " + (FinalBalance + BonusRubles) + " рублей.");
+            System.out.println("Ваши бонусы составят: " + bonusRubles + " рублей.");
+            System.out.println("Ваш итоговый баланс равен: " + (finalBalance + bonusRubles) + " рублей.");
         } else {
 
             System.out.println("Бонусов нет, вы не выполнили условия акции !");
-            System.out.println("Ваш итоговый баланс равен: " + FinalBalance + " рублей.");
+            System.out.println("Ваш итоговый баланс равен: " + finalBalance + " рублей.");
 
         }
 
